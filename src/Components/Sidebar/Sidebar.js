df111c7b9525ce598df67bc9c65547adb168d973
Boone Epstein <booneepstein@gmail.com>
2021-02-05 23:03:31 -0600

    Build file structure for components

---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <h2>hi</h2>
+  )
+}
+
+export default Sidebar
---
+++ src/Components/TripList/TripList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TripList = () => {
+  return (
+    <h2>hi</h2>
+  )
+}
+
+export default TripList
