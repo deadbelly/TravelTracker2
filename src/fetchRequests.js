2ce2e839e66cdd457261b46fc10a0ce99b7b9e95
Boone Epstein <booneepstein@gmail.com>
2021-02-05 23:04:51 -0600

    Add fetch request methods

---
+++ src/fetchRequests.js
@@ -0,0 +1,58 @@
+const fetchRequests = {
+  getTraveler(id) {
+    return fetch(`http://localhost:3001/api/v1/travelers/${id}`)
+      .then(response => response.json())
+  },
+
+  getAllTravelers() {
+    return fetch(`http://localhost:3001/api/v1/travelers`)
+      .then(response => response.json())
+  },
+
+  getTrips() {
+    return fetch('http://localhost:3001/api/v1/trips')
+      .then(response => response.json())
+  },
+
+  getDestinations() {
+    return fetch('http://localhost:3001/api/v1/destinations')
+      .then(response => response.json())
+  },
+
+  getAllData(id) {
+    return [fetchRequests.getTraveler(id), fetchRequests.getTrips(), fetchRequests.getDestinations()];
+  },
+
+  // getAgentData() {
+  //   return [fetchRequests.getAllTravelers(), fetchRequests.getTrips(), fetchRequests.getDestinations()];
+  // },
+  //
+  // postTrip(obj) {
+  //   return fetch('http://localhost:3001/api/v1/trips', {
+  //     method: 'POST',
+  //     headers: {'Content-Type': 'application/json'},
+  //     body: JSON.stringify(obj)
+  //   })
+  //     .then(response => response.json())
+  //     .catch(err => domUpdates.displayError(err, 'form'));
+  // },
+  //
+  // deleteTrip(id) {
+  //   fetch(`http://localhost:3001/api/v1/trips/${id}`, {
+  //     method: 'DELETE',
+  //     headers: {'Content-Type': 'application/json'},
+  //   })
+  // },
+  //
+  // approveAndModifyTrip(obj) {
+  //   return fetch('http://localhost:3001/api/v1/updateTrip', {
+  //     method: 'POST',
+  //     headers: {'Content-Type': 'application/json'},
+  //     body: JSON.stringify(obj)
+  //   })
+  //     .then(response => response.json())
+  //     .catch(err => domUpdates.displayError(err, 'agent'));
+  // }
+}
+
+export default fetchRequests
